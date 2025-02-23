fcaa377b2591ea9cf2e6cb319c546d3fd7a335f2
mtp1999 <mohamad.tavakolpoorsaleh@gmail.com>
2025-02-23 18:08:22 +0330

    season6 (learning generic api views)
    implementing views using generic api views in app_blog

--- backend/app_blog/api/v1/views.py
+++ backend/app_blog/api/v1/views.py
@@ -1,45 +1,16 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import permissions
-from django.shortcuts import get_object_or_404
 from app_blog.models import Post
 from app_blog.api.v1.serializers import PostSer
 
 
-class PostListView(APIView):
+class PostListView(ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSer
-
-    def get(self, request, format=None):
-        posts = Post.objects.filter(status=True)
-        serializer = self.serializer_class(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response('Ok')
+    queryset = Post.objects.filter(status=True)
 
 
-class PostDetailView(APIView):
+class PostDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSer
-
-    def get(self, request, pk, format=None):
-        post = get_object_or_404(Post, id=pk, status=True)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = get_object_or_404(Post, id=pk, status=True)
-        serializer = self.serializer_class(post, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response('Ok')
-
-    def delete(self, request, pk, format=None):
-        post = get_object_or_404(Post, id=pk, status=True)
-        post.delete()
-        return Response({'detail': 'post deleted!'}, status=status.HTTP_204_NO_CONTENT)
+    queryset = Post.objects.filter(status=True)
